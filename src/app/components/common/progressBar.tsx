4a7038a7bd0cf5bfd0d35cc496f456832e643347
In Ho (Luke) <seoluke7203@hotmail.com>
2025-01-19 13:42:55 +0900

    Coming Soon display ready

--- src/app/components/common/progressBar.tsx
+++ src/app/components/common/progressBar.tsx
@@ -1,9 +1,7 @@
 export default function ProgressBar({ width = 0 }: { width?: number }) {
-
   return (
     <div className="bg-gray-200 rounded-full h-1.5 dark:bg-baase-100">
       <div id="progressBar"className="max-w-full glass bg-primary h-1.5 rounded-full dark:bg-accent" style={ { width: `${width}%` } }></div>
     </div>
   )
-
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,10 +4,16 @@ import Header from '@/app/components/common/header'
 export default function Page() {
   return (
     <div>
-      <div id="top" className='sticky top-0 z-50'>
+      <div id="top" className="sticky top-0 z-50">
         <Header />
       </div>
-      <div>Hello! this is contact page</div>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <div className="text-center p-8 bg-white shadow-md rounded-md">
+          <h1 className="text-3xl font-bold mb-4 text-base-300">준비중입니다</h1>
+          <p className="text-lg mb-2 text-base-300">This site is currently under construction.</p>
+          <p className="text-lg text-base-300">곧 다시 찾아뵙겠습니다!</p>
+        </div>
+      </div>
       <Footer />
     </div>
   )
